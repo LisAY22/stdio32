70e10c02f8db7e45e5b896d55ff8c76c87f099e2
Elisa Ajxup Yax <lisajxupyax@gmail.com>
2024-04-07 17:24:02 -0600

    Correcciones

--- invertir-cadena.asm
+++ invertir-cadena.asm
@@ -1,33 +1,71 @@
 ; Invertir cadena
-; Autor: lis
-; Fecha: 20240306
+; autor: lis
+; fecha: 20240306
 
 %include 'stdio32.asm'
 
 SECTION .data
-	msg	 db      'Ingrese una cadena: ', 0
-        msg2     db      'Cadena invertida: ', 0
+        msg1        db      'Ingrese la cadena a invertir: ', 0
+        msg2        db      'Cadena invertida: ', 0
+
+        len1        equ     $-msg1
+        len2        equ     $-msg2
 
 SECTION .bss
-	cadena             	resb        100
-        cadenaInvertida		resb        100
+        cadena              resb        100
+        cadenaInvertida     resb        100
 
 SECTION .text
         global _start
 
 _start:
-	mov	eax, msg
-	call 	strPrint
+        mov     eax, msg1
+        call    strPrint
+        mov     edx, len1
 
-	mov 	eax, cadena
-	mov	esi, cadena
-	mov 	edi, cadenaInvertida
-	call	strInput
+        mov     eax, 3
+        mov     ebx, 0
+        mov     ecx, cadena
+        mov     edx, 100
+        int     80h
 
-	mov	eax, cadena
-	call 	strInvert
+        mov     esi, cadena
+        call    strLen2
 
-	mov	eax, cadenaInvertida
-	call	strPrintLn
+	mov     esi, cadena
+        mov     edi, cadenaInvertida
+        add     edi, eax
+        call    reverseString
 
-	call	Quit
+        mov     eax, msg2
+        call    strPrint
+        mov     edx, len2
+
+        mov     eax, cadenaInvertida
+        call    strPrintLn
+
+        call    Quit
+
+strLen2:
+        xor     ecx, ecx
+CicloLen:
+        cmp     byte [esi + ecx], 0
+        je      returnLen
+        inc     ecx
+        jmp     CicloLen
+
+returnLen:
+        mov     eax, ecx
+        ret
+
+reverseString:
+        mov     edx, eax
+        dec     edi
+
+CicloReverse:
+        mov     al, [esi]
+        mov     [edi], al
+        inc     esi
+        dec     edi
+        loop    CicloReverse
+        ret
